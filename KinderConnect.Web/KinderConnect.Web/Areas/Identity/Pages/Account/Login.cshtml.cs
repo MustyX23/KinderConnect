efb5aa672a702d08487751d31c248b0b7471d3bb
Mustafa Buhov <mustyx900@gmail.com>
2024-03-01 11:18:19 +0200

    Login and Register logic is changed
    --A user can now login with his username, instead of password.
    --When registering a new user it is mandatory for him to input a username.

--- KinderConnect.Web/KinderConnect.Web/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ KinderConnect.Web/KinderConnect.Web/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -54,13 +54,13 @@ namespace KinderConnect.Web.Areas.Identity.Pages.Account
         /// </summary>
         public class InputModel
         {
+            [Required]
+            public string Username { get; set; }
             /// <summary>
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
-            [Required]
-            [EmailAddress]
-            public string Email { get; set; }
+            /// 
 
             /// <summary>
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
@@ -105,7 +105,7 @@ namespace KinderConnect.Web.Areas.Identity.Pages.Account
             {
                 // This doesn't count login failures towards account lockout
                 // To enable password failures to trigger account lockout, set lockoutOnFailure: true
-                var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, lockoutOnFailure: false);
+                var result = await _signInManager.PasswordSignInAsync(Input.Username, Input.Password, Input.RememberMe, lockoutOnFailure: false);
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User logged in.");
--- KinderConnect.Web/KinderConnect.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ KinderConnect.Web/KinderConnect.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -73,6 +73,10 @@ namespace KinderConnect.Web.Areas.Identity.Pages.Account
             [Display(Name = "Email")]
             public string Email { get; set; }
 
+            [Required]
+            [Display(Name = "Username")]
+            public string Username { get; set; }
+
             /// <summary>
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
@@ -108,7 +112,7 @@ namespace KinderConnect.Web.Areas.Identity.Pages.Account
             {
                 var user = CreateUser();
 
-                await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
+                await _userStore.SetUserNameAsync(user, Input.Username, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
