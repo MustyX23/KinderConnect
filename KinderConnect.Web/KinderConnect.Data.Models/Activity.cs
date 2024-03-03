7334c978c809d10fffbc6c8f3b1a7633e0ebd6de
Mustafa Buhov <mustyx900@gmail.com>
2024-03-03 18:36:04 +0200

    Activity&AttendanceRecord Entities - Added
    -- Added two additional entites so can extend the logic of the application

---
+++ KinderConnect.Web/KinderConnect.Data.Models/Activity.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static KinderConnect.Common.EntityValidationConstants.Activity;
+
+namespace KinderConnect.Data.Models
+{
+    public class Activity
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(NameMaxLength, MinimumLength = NameMinLength)]
+        [Display(Name = "Activity")]
+        public string Name { get; set; } = null!;
+    }
+}
---
+++ KinderConnect.Web/KinderConnect.Data.Models/AttendanceRecord.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KinderConnect.Data.Models
+{
+    public class AttendanceRecord
+    {
+        public AttendanceRecord()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; set; }
+
+        [Required]
+        public Guid ChildId { get; set; }
+
+        [Required]
+        public Guid TeacherId { get; set; }
+
+        [Required]
+        public DateTime Start { get; set; }
+
+        [Required]
+        public TimeSpan End { get; set; }
+
+        [Required]
+        public int ActivityId { get; set; }       
+
+        public string? Comment { get; set; }
+
+        [Required]
+        public Child Child { get; set; } = null!;
+
+        [Required]
+        public Teacher Teacher { get; set; } = null!;
+
+        [Required]
+        public Activity Activity { get; set; } = null!;
+
+    }
+}
