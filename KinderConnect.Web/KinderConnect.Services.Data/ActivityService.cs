b478d93f899f618352ee2306b20dd9572d88aa7d
Mustafa Buhov <mustyx900@gmail.com>
2024-03-21 17:07:12 +0200

    Service Naming
    -- Named most of the services we are going to need for the system

---
+++ KinderConnect.Web/KinderConnect.Services.Data/ActivityService.cs
@@ -0,0 +1,8 @@
+﻿using KinderConnect.Services.Data.Interfaces;
+
+namespace KinderConnect.Services.Data
+{
+    public class ActivityService : IActivityService
+    {
+    }
+}
---
+++ KinderConnect.Web/KinderConnect.Services.Data/Interfaces/IActivityService.cs
@@ -0,0 +1,6 @@
+﻿namespace KinderConnect.Services.Data.Interfaces
+{
+    public interface IActivityService
+    {
+    }
+}
