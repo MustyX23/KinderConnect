67442a0de16bb169d1a6763c6a2ddeab44301fbb
Mustafa Buhov <mustyx900@gmail.com>
2024-03-21 17:07:51 +0200

    Controller Naming
    --Named all the controllers we are going to need for our system.

---
+++ KinderConnect.Web/KinderConnect.Web/Controllers/ChildController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KinderConnect.Web.Controllers
+{
+    public class ChildController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ KinderConnect.Web/KinderConnect.Web/Controllers/ClassroomController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KinderConnect.Web.Controllers
+{
+    public class ClassroomController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
