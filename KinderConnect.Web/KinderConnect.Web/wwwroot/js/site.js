a5fe3f0826ac6649881ab8e488339716d00b171f
Mustafa Buhov <mustyx900@gmail.com>
2024-03-31 18:53:25 +0300

    Slight change for the _IndexHeadPartial
    -- Also a slight change for the show/hide content function

--- KinderConnect.Web/KinderConnect.Web/wwwroot/js/site.js
+++ KinderConnect.Web/KinderConnect.Web/wwwroot/js/site.js
@@ -1,6 +1,22 @@
 ﻿$(document).ready(function () {
     $('.toggle-content').click(function () {
-        var parent = $(this).closest('.card-body');
-        parent.find('.content-summary, .content-full, .toggle-content').toggle();
+        // Find the parent card body of the clicked button
+        var parentCardBody = $(this).closest('.card-body');
+
+        // Check if the parent card body exists
+        if (parentCardBody.length > 0) {
+            // Toggle the visibility of content-summary and content-full within the same card body
+            parentCardBody.find('.content-summary, .content-full').toggle();
+        } else {
+            // Find the parent container of the clicked button
+            var parentContainer = $(this).closest('.row');
+
+            // Toggle the visibility of content-summary and content-full within the same container
+            parentContainer.find('.content-summary, .content-full').toggle();
+        }
+
+        // Toggle the visibility of the clicked button and its sibling
+        $(this).toggle();
+        $(this).siblings('.toggle-content').toggle();
     });
 });
