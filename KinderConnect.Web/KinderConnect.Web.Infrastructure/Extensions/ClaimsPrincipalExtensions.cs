1ec5aa8898cf9842255a395a80439bd2a66c8232
Mustafa Buhov <mustyx900@gmail.com>
2024-04-03 01:48:39 +0300

    Create ClaimsPrincipalExtensions.cs

---
+++ KinderConnect.Web/KinderConnect.Web.Infrastructure/Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Claims;
+
+namespace KinderConnect.Web.Infrastructure.Extensions
+{
+    public static class ClaimsPrincipalExtensions
+    {
+        public static string GetUserId(this ClaimsPrincipal user)
+        {
+            return user.FindFirstValue(ClaimTypes.NameIdentifier);
+        }
+    }
+}
