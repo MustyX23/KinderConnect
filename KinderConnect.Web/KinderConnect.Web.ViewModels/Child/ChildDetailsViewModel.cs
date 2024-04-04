f54d7eef0cbc1f0d41ce66e36eca22d0966cbdf9
Mustafa Buhov <mustyx900@gmail.com>
2024-04-04 21:10:02 +0300

    Create ChildDetailsViewModel.cs

---
+++ KinderConnect.Web/KinderConnect.Web.ViewModels/Child/ChildDetailsViewModel.cs
@@ -0,0 +1,30 @@
+﻿using KinderConnect.Web.ViewModels.Classroom;
+
+namespace KinderConnect.Web.ViewModels.Child
+{
+    public class ChildDetailsViewModel
+    {
+        public string Id {  get; set; } = null!;
+        public string FirstName { get; set; } = null!;
+
+        public string LastName { get; set; } = null!;
+
+        public int Age { get; set; }
+
+        public string Gender { get; set; } = null!;
+
+        public string ImageUrl { get; set; } = null!;
+
+        public bool IsActive { get; set; }
+
+        public string ParentGuardianContact { get; set; } = null!;
+        public string? MedicalInformation { get; set; }
+
+        public string? Allergies { get; set; }
+
+        public string DateOfBirth { get; set; } = null!;
+
+        public IEnumerable<DetailsClassroomViewModel> Classrooms { get; set; } =
+            new HashSet<DetailsClassroomViewModel>();
+    }
+}
