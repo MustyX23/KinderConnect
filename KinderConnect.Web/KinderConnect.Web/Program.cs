b3a4bbcee098182ef59d6771639515dd546d6365
Mustafa Buhov <mustyx900@gmail.com>
2024-04-09 16:36:06 +0300

    Update Program.cs

--- KinderConnect.Web/KinderConnect.Web/Program.cs
+++ KinderConnect.Web/KinderConnect.Web/Program.cs
@@ -56,9 +56,13 @@ namespace KinderConnect.Web
             app.UseAuthentication();
             app.UseAuthorization();
 
-            app.MapControllerRoute(
-                name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
+            });
 
             app.MapRazorPages();
 
