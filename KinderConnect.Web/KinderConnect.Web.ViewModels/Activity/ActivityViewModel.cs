ea3e408724ff12357d5c1f6c4754e6a94c188af5
Mustafa Buhov <mustyx900@gmail.com>
2024-04-13 20:00:52 +0300

    Create ActivityViewModel.cs

---
+++ KinderConnect.Web/KinderConnect.Web.ViewModels/Activity/ActivityViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace KinderConnect.Web.ViewModels.Activity
+{
+    public class ActivityViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+    }
+}
