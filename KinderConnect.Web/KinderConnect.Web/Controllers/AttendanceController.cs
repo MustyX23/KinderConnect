18222bb731c831f8d2e747da9bbc89bf6f8d92ca
Mustafa Buhov <mustyx900@gmail.com>
2024-04-13 19:59:36 +0300

    AttendanceController is now authorized

--- KinderConnect.Web/KinderConnect.Web/Controllers/AttendanceController.cs
+++ KinderConnect.Web/KinderConnect.Web/Controllers/AttendanceController.cs
@@ -1,12 +1,38 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using KinderConnect.Services.Data.Interfaces;
+using KinderConnect.Web.ViewModels.Attendance;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace KinderConnect.Web.Controllers
 {
+    [Authorize]
     public class AttendanceController : Controller
     {
-        public IActionResult Index()
+        private IAttendanceService attendanceService;
+
+        public AttendanceController(IAttendanceService attendanceService)
         {
-            return View();
+            this.attendanceService = attendanceService;
+        }
+        public async Task<IActionResult> AttendanceRecords(string teacherId, string classroomId)
+        {
+            IEnumerable<AttendanceRecordFormModel> attendanceRecords
+                = await attendanceService.GetAllAttendancesByTeacherAndClassroomIdAsync(teacherId, classroomId);
+
+            return View(attendanceRecords);
+        }
+        public async Task<IActionResult> Create(string classroomId)
+        {
+            var viewModel
+                = await attendanceService
+                .GetAttendanceRecordFormModelByClassroomIdAsync(classroomId);
+
+            return View(viewModel);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(AttendanceRecordFormModel model)
+        {
+            return Ok();
         }
     }
 }
