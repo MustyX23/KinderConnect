c358f277eb22ea442d463243af624b8976fb6b15
Mustafa Buhov <mustyx900@gmail.com>
2024-04-13 20:01:53 +0300

    Create ViewModels to display Teacher's Classrooms and Child for Attendances

---
+++ KinderConnect.Web/KinderConnect.Web.ViewModels/Child/ChildForAttendanceViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace KinderConnect.Web.ViewModels.Child
+{
+    public class ChildForAttendanceViewModel
+    {
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string FullName { get; set; }
+
+        public bool IsPresent { get; set; }
+    }
+}
---
+++ KinderConnect.Web/KinderConnect.Web.ViewModels/Teacher/MyClassroomViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace KinderConnect.Web.ViewModels.Teacher
+{
+    public class MyClassroomViewModel
+    {
+        public string TeacherId { get; set; } = null!;
+        public string TeacherFirstName { get; set; } = null!;
+        public string ClassroomId { get; set; } = null!;
+        public string ClassroomName { get; set; } = null!;
+        public string ClassroomImageUrl { get; set; } = null!;
+    }
+}
