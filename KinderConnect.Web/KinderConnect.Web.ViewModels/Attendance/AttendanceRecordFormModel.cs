f4429d37695f8f88d7da8a5711e6a85dd3c07598
Mustafa Buhov <mustyx900@gmail.com>
2024-04-13 20:00:54 +0300

    Create AttendanceRecordFormModel.cs

---
+++ KinderConnect.Web/KinderConnect.Web.ViewModels/Attendance/AttendanceRecordFormModel.cs
@@ -0,0 +1,29 @@
+﻿using KinderConnect.Web.ViewModels.Activity;
+using KinderConnect.Web.ViewModels.Child;
+
+namespace KinderConnect.Web.ViewModels.Attendance
+{
+    public class AttendanceRecordFormModel
+    {
+        public string Id { get; set; }
+        public string ClassroomId {  get; set; }
+
+        public string TeacherId { get; set; }
+
+        public string Start { get; set; }
+
+        public string End { get; set; }
+
+        public int ActivityId { get; set; }
+
+        public string Actvity { get; set; }
+
+        public string? Comment { get; set; }
+
+        public List<ChildForAttendanceViewModel> Children { get; set; }
+            = new List<ChildForAttendanceViewModel>();
+
+        public List<ActivityViewModel> Activities { get; set; }
+            = new List<ActivityViewModel>();
+    }
+}
