191f004c5e87c4296828b3704d17165f4314e72a
Mustafa Buhov <mustyx900@gmail.com>
2024-04-14 20:48:28 +0300

    Update AttendanceController.cs
    -- Create and Edit attendance is now available in the AttendanceController.
    -- Most validational part is implemented.

--- KinderConnect.Web/KinderConnect.Web/Controllers/AttendanceController.cs
+++ KinderConnect.Web/KinderConnect.Web/Controllers/AttendanceController.cs
@@ -1,7 +1,10 @@
-﻿using KinderConnect.Services.Data.Interfaces;
+﻿using KinderConnect.Services.Data;
+using KinderConnect.Services.Data.Interfaces;
+using KinderConnect.Web.Infrastructure.Extensions;
 using KinderConnect.Web.ViewModels.Attendance;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using static KinderConnect.Common.NotificationMessagesConstants;
 
 namespace KinderConnect.Web.Controllers
 {
@@ -9,16 +12,48 @@ namespace KinderConnect.Web.Controllers
     public class AttendanceController : Controller
     {
         private IAttendanceService attendanceService;
+        private ITeacherService teacherService;
 
-        public AttendanceController(IAttendanceService attendanceService)
+        public AttendanceController(
+            IAttendanceService attendanceService,
+            ITeacherService teacherService)
         {
             this.attendanceService = attendanceService;
+            this.teacherService = teacherService;
+
         }
         public async Task<IActionResult> AttendanceRecords(string teacherId, string classroomId)
         {
+            string userId = User.GetUserId();
+
+            bool isTeacher = await teacherService.IsTeacherByUserIdAsync(userId);
+            bool isTeacherLeader = await teacherService.IsTeacherLeaderOfClassroomByIdAndClassroomIdAsync(teacherId, classroomId);
+
+            if (!isTeacher)
+            {
+                ModelState.AddModelError(string.Empty, "You don't have permission to access this page.");
+                TempData[ErrorMessage] = $"You don't have permission to access this page.";
+                return RedirectToAction("Index", "Home");
+            }
+            if (teacherId == null)
+            {
+                ModelState.AddModelError(string.Empty, "You don't have permission to access this page.");
+                TempData[ErrorMessage] = $"You don't have permission to access this page.";
+                return RedirectToAction("Index", "Home");
+            }
+            if (!isTeacherLeader)
+            {
+                ModelState.AddModelError(string.Empty, "You don't have permission to access this page.");
+                TempData[ErrorMessage] = $"You don't have permission to access this page.";
+                return RedirectToAction("Index", "Home");
+            }
             IEnumerable<AttendanceRecordFormModel> attendanceRecords
                 = await attendanceService.GetAllAttendancesByTeacherAndClassroomIdAsync(teacherId, classroomId);
 
+            if (classroomId != null)
+            {
+                ViewBag.ClassroomId = classroomId;
+            }           
             return View(attendanceRecords);
         }
         public async Task<IActionResult> Create(string classroomId)
@@ -32,8 +67,60 @@ namespace KinderConnect.Web.Controllers
         [HttpPost]
         public async Task<IActionResult> Create(AttendanceRecordFormModel model)
         {
-            await attendanceService.CreateAttendanceRecordAsync(model);
+            string userId = User.GetUserId();
+
+            bool isTeacher = await teacherService.IsTeacherByUserIdAsync(userId);
+            string teacherId = await teacherService.GetTeacherIdByUserIdAsync(userId);
+
+            if (!isTeacher)
+            {
+                ModelState.AddModelError(string.Empty, "You don't have permission to access this page.");
+                TempData[ErrorMessage] = $"You don't have permission to access this page.";
+                return RedirectToAction("Index", "Home");
+            }
+            if (teacherId == null)
+            {
+                ModelState.AddModelError(string.Empty, "You don't have permission to access this page.");
+                TempData[ErrorMessage] = $"You don't have permission to access this page.";
+                return RedirectToAction("Index", "Home");
+            }
+
+
+            await attendanceService.CreateAttendanceRecordAsync(model, teacherId);
             return RedirectToAction("AttendanceRecords", "Attendance");
+        }
+        [HttpPost]
+        public async Task<IActionResult> EditPresence(string attendanceId, string childId)
+        {
+            string userId = User.GetUserId();
+
+            bool isTeacher = await teacherService.IsTeacherByUserIdAsync(userId);
+            string teacherId = await teacherService.GetTeacherIdByUserIdAsync(userId);
+            string classroomId = await attendanceService.GetClassroomIdByAttendancyIdAsync(attendanceId);
+            bool isTeacherLeader = await teacherService.IsTeacherLeaderOfClassroomByIdAndClassroomIdAsync(teacherId, classroomId);
+
+            if (!isTeacher)
+            {
+                ModelState.AddModelError(string.Empty, "You don't have permission to access this page.");
+                TempData[ErrorMessage] = $"You don't have permission to access this page.";
+                return RedirectToAction("Index", "Home");
+            }
+            if (teacherId == null)
+            {
+                ModelState.AddModelError(string.Empty, "You don't have permission to access this page.");
+                TempData[ErrorMessage] = $"You don't have permission to access this page.";
+                return RedirectToAction("Index", "Home");
+            }
+            if (!isTeacherLeader)
+            {
+                ModelState.AddModelError(string.Empty, "You don't have permission to access this page.");
+                TempData[ErrorMessage] = $"You don't have permission to access this page.";
+                return RedirectToAction("Index", "Home");
+            }
+
+            await attendanceService.EditPresenceAsync(attendanceId, childId);
+            TempData[SuccessMessage] = $"You have succesfully changed child's presence";
+            return RedirectToAction("AttendanceRecords", "Attendance" ,new {teacherId, classroomId });
         }
     }
 }
