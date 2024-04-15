8791dfa6ba06ae314718688bdbded019ed269dbc
Mustafa Buhov <mustyx900@gmail.com>
2024-04-15 07:55:02 +0300

    Create ClassroomSorting.cs

---
+++ KinderConnect.Web/KinderConnect.Web.ViewModels/Classroom/Enums/ClassroomSorting.cs
@@ -0,0 +1,10 @@
+﻿namespace KinderConnect.Web.ViewModels.Classroom.Enums
+{
+    public enum ClassroomSorting
+    {
+        Newest = 0,
+        Oldest = 1,
+        ElderAge = 2,
+        YoungerAge = 3,
+    }
+}
