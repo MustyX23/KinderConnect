baab004c04cd20b6d5b104a0610c4f7c0b8eda7a
Mustafa Buhov <mustyx900@gmail.com>
2024-04-15 07:52:49 +0300

    Create AllClassroomsQueryModel.cs
    -- We need this property in order to create a pagination on the classroom index page.

---
+++ KinderConnect.Web/KinderConnect.Web.ViewModels/Classroom/AllClassroomsQueryModel.cs
@@ -0,0 +1,24 @@
+﻿using KinderConnect.Web.ViewModels.Classroom.Enums;
+using System.ComponentModel.DataAnnotations;
+
+using static KinderConnect.Common.GeneralApplicationConstants;
+namespace KinderConnect.Web.ViewModels.Classroom
+{
+    public class AllClassroomsQueryModel
+    {
+        [Display(Name = "Search by text")]
+        public string? SearchString { get; set; }
+
+        [Display(Name = "Sort Classes By:")]
+        public ClassroomSorting ClassroomSorting { get; set; }
+
+        public int CurrentPage { get; set; } = DefaultPage;
+
+        [Display(Name = "Show Classes on Page")]
+        public int ClassesPerPage { get; set; } = EntitiesPerPage;
+
+        public int TotalClasses { get; set; }
+
+        public IEnumerable<AllClassroomViewModel> Classes { get; set; } = new HashSet<AllClassroomViewModel>();
+    }
+}
