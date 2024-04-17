9f532f160cad1b0848560a8a321fef65d38cd192
Mustafa Buhov <mustyx900@gmail.com>
2024-04-17 20:54:23 +0300

    UI Update: Remove all unnesacarry assets

--- KinderConnect.Web/KinderConnect.Web/wwwroot/js/main.js
+++ KinderConnect.Web/KinderConnect.Web/wwwroot/js/main.js
@@ -33,70 +33,70 @@
     });
 
 
-//    // Portfolio isotope and filter
-//    var portfolioIsotope = $('.portfolio-container').isotope({
-//        itemSelector: '.portfolio-item',
-//        layoutMode: 'fitRows'
-//    });
+    // Portfolio isotope and filter
+    var portfolioIsotope = $('.portfolio-container').isotope({
+        itemSelector: '.portfolio-item',
+        layoutMode: 'fitRows'
+    });
 
-//    $('#portfolio-flters li').on('click', function () {
-//        $("#portfolio-flters li").removeClass('active');
-//        $(this).addClass('active');
+    $('#portfolio-flters li').on('click', function () {
+        $("#portfolio-flters li").removeClass('active');
+        $(this).addClass('active');
 
-//        portfolioIsotope.isotope({filter: $(this).data('filter')});
-//    });
-
-
-//    // Post carousel
-//    $(".post-carousel").owlCarousel({
-//        autoplay: true,
-//        smartSpeed: 1500,
-//        dots: false,
-//        loop: true,
-//        nav : true,
-//        navText : [
-//            '<i class="fa fa-angle-left" aria-hidden="true"></i>',
-//            '<i class="fa fa-angle-right" aria-hidden="true"></i>'
-//        ],
-//        responsive: {
-//            0:{
-//                items:1
-//            },
-//            576:{
-//                items:1
-//            },
-//            768:{
-//                items:2
-//            },
-//            992:{
-//                items:2
-//            }
-//        }
-//    });
+        portfolioIsotope.isotope({filter: $(this).data('filter')});
+    });
 
 
-//    // Testimonials carousel
-//    $(".testimonial-carousel").owlCarousel({
-//        center: true,
-//        autoplay: true,
-//        smartSpeed: 2000,
-//        dots: true,
-//        loop: true,
-//        responsive: {
-//            0:{
-//                items:1
-//            },
-//            576:{
-//                items:1
-//            },
-//            768:{
-//                items:2
-//            },
-//            992:{
-//                items:3
-//            }
-//        }
-//    });
+    // Post carousel
+    $(".post-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1500,
+        dots: false,
+        loop: true,
+        nav : true,
+        navText : [
+            '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+            '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+        ],
+        responsive: {
+            0:{
+                items:1
+            },
+            576:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            992:{
+                items:2
+            }
+        }
+    });
+
+
+    // Testimonials carousel
+    $(".testimonial-carousel").owlCarousel({
+        center: true,
+        autoplay: true,
+        smartSpeed: 2000,
+        dots: true,
+        loop: true,
+        responsive: {
+            0:{
+                items:1
+            },
+            576:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            992:{
+                items:3
+            }
+        }
+    });
     
-//})(jQuery);
+})(jQuery);
 
