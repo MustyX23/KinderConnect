f39d0ba7603d9d997459c3636683cc7fe9634ed8
Mustafa Buhov <mustyx900@gmail.com>
2024-04-17 19:54:14 +0300

    Introducing Error Page Handling

--- KinderConnect.Web/KinderConnect.Web/Controllers/HomeController.cs
+++ KinderConnect.Web/KinderConnect.Web/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using KinderConnect.Web.Models;
 using KinderConnect.Web.ViewModels.Home;
 using KinderConnect.Web.ViewModels.Teacher;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -67,9 +68,23 @@ namespace KinderConnect.Web.Controllers
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        public IActionResult Error(int statusCode)
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            if (statusCode == 400 || statusCode == 404)
+            {
+                return View("Error404");
+            }
+            if (statusCode == 500)
+            {
+                return View("Error500");
+            }
+
+            if (statusCode == 401)
+            {
+                return View("Error401");
+            }
+
+            return View();
         }
     }
 }
--- KinderConnect.Web/KinderConnect.Web/Program.cs
+++ KinderConnect.Web/KinderConnect.Web/Program.cs
@@ -57,12 +57,15 @@ namespace KinderConnect.Web
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
+                app.UseDeveloperExceptionPage();
                 app.UseMigrationsEndPoint();
             }
             else
             {
+                app.UseExceptionHandler("/Home/Error/500");
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseStatusCodePagesWithRedirects("/Home/Error?statusCode={0}");
+                
                 app.UseHsts();
             }
 
